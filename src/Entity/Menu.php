31fce528fc95fd4fcf0fe0e14cb57f391fb0042b
JohLav <[johana.lavigne@gmail.com](mailto:johana.lavigne@gmail.com>
2023-04-18 14:21:17 +0200

    Created entities without the relations

---
+++ src/Entity/Menu.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MenuRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: MenuRepository::class)]
+class Menu
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $description = null;
+
+    #[ORM\Column(type: Types::DECIMAL, precision: 5, scale: 2)]
+    private ?string $reduction = null;
+
+    #[ORM\ManyToOne]
+    private ?Restaurant $restaurant = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getReduction(): ?string
+    {
+        return $this->reduction;
+    }
+
+    public function setReduction(string $reduction): self
+    {
+        $this->reduction = $reduction;
+
+        return $this;
+    }
+
+    public function getRestaurant(): ?Restaurant
+    {
+        return $this->restaurant;
+    }
+
+    public function setRestaurant(?Restaurant $restaurant): self
+    {
+        $this->restaurant = $restaurant;
+
+        return $this;
+    }
+}
---
+++ src/Entity/Reservation.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ReservationRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ReservationRepository::class)]
+class Reservation
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $datetime = null;
+
+    #[ORM\Column(length: 45, nullable: true)]
+    private ?string $payment_mode = null;
+
+    #[ORM\Column]
+    private ?int $adult_nb = null;
+
+    #[ORM\Column]
+    private ?int $kid_nb = null;
+
+    #[ORM\Column(length: 45)]
+    private ?string $service = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDatetime(): ?\DateTimeInterface
+    {
+        return $this->datetime;
+    }
+
+    public function setDatetime(\DateTimeInterface $datetime): self
+    {
+        $this->datetime = $datetime;
+
+        return $this;
+    }
+
+    public function getPaymentMode(): ?string
+    {
+        return $this->payment_mode;
+    }
+
+    public function setPaymentMode(?string $payment_mode): self
+    {
+        $this->payment_mode = $payment_mode;
+
+        return $this;
+    }
+
+    public function getAdultNb(): ?int
+    {
+        return $this->adult_nb;
+    }
+
+    public function setAdultNb(int $adult_nb): self
+    {
+        $this->adult_nb = $adult_nb;
+
+        return $this;
+    }
+
+    public function getKidNb(): ?int
+    {
+        return $this->kid_nb;
+    }
+
+    public function setKidNb(int $kid_nb): self
+    {
+        $this->kid_nb = $kid_nb;
+
+        return $this;
+    }
+
+    public function getService(): ?string
+    {
+        return $this->service;
+    }
+
+    public function setService(string $service): self
+    {
+        $this->service = $service;
+
+        return $this;
+    }
+}
