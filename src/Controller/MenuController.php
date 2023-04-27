3f115a868dcbfe094ed28a1aee0253ad6cfb1fb1
JohLav <[johana.lavigne@gmail.com](mailto:johana.lavigne@gmail.com>
2023-04-27 17:01:35 +0200

    Correction agenda route error.

--- src/Controller/MenuController.php
+++ src/Controller/MenuController.php
@@ -14,7 +14,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/agenda', name: 'dashboard_')]
+#[Route('/dashboard', name: 'dashboard_')]
 class MenuController extends AbstractController
 {
     public function __construct(
