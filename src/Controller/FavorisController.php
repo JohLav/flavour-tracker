d1b1f51c0544ac63f0d63b93dacc29e13dac3d68
Lervy Myriam <[javamyriam@gmail.com](mailto:javamyriam@gmail.com)>
2023-05-05 10:16:09 +0200

    Form restaurant

--- src/Controller/FavorisController.php
+++ src/Controller/FavorisController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Restaurant;
+use App\Entity\User;
+use Composer\DependencyResolver\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,8 +14,28 @@ class FavorisController extends AbstractController
     #[Route('/favoris', name: 'app_favoris')]
     public function index(): Response
     {
-        return $this->render('favoris/index.html.twig', [
+        return $this->render('favoris/favoris.html.twig', [
             'controller_name' => 'Favoris',
         ]);
+    }
+
+    #[Route('/favoris/new/{id}', name: 'app_favoris_new')]
+    public function addFavoris(Restaurant $restaurant): Response
+    {
+        /** @var User $user */
+        $user = $this->getUser();
+        $user->addFavorites($restaurant);
+
+        return $this->redirectToRoute('app_favoris');
+    }
+
+    #[Route('/favoris/remove/{id}', name: 'app_favoris_remove')]
+    public function removeFavoris(Restaurant $restaurant): Response
+    {
+        /** @var User $user */
+        $user = $this->getUser();
+        $user->removeFavorites($restaurant);
+
+        return $this->redirectToRoute('app_favoris');
     }
 }
--- src/Form/RestaurantType.php
+++ src/Form/RestaurantType.php
@@ -2,7 +2,9 @@
 
 namespace App\Form;
 
+use App\Entity\Category;
 use App\Entity\Restaurant;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -17,18 +19,29 @@ class RestaurantType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name', type: TextType::class)
-            ->add('address', type: TextareaType::class)
-            ->add('city', type: TextType::class)
-            ->add('zip_code', type: TextType::class)
-            ->add('phone', type: TextType::class)
-            ->add('capacity', type: TextType::class)
-            ->add('category', type: TextType::class)
-            ->add('users', type: TextType::class)
+            ->add('name', TextType::class, [
+            'label' => 'Name',
+            'attr' => ['class' => 'item'],
+           ])
+
+            ->add('address', TextareaType::class)
+            ->add('city', TextType::class)
+            ->add('zip_code', TextType::class)
+            ->add('phone', TextType::class)
+            ->add('capacity', TextType::class)
+            ->add('category', EntityType::class)
+            ->add('users', TextType::class)
+
+            ->add('category', EntityType::class, [
+                'class' => Category::class,
+                'choice_label' => 'name',
+            ])
             ->add('images', FileType::class, [
                 'multiple' => true,
                 'mapped' => false,
             ])
+
+
             ->add('save', SubmitType::class)
         ;
     }
