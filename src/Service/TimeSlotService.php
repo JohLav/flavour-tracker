6ebd08487162665409429ac0fa277d49c1db6314
Guillaume <cvr.contact.pro@gmail.com>
2023-05-09 21:40:44 +0200

    Count place

--- src/Service/TimeSlotService.php
+++ src/Service/TimeSlotService.php
@@ -6,28 +6,27 @@ use App\Entity\Restaurant;
 use App\Repository\ReservationRepository;
 use App\Repository\RestaurantRepository;
 use App\Repository\TimeSlotRepository;
+use DateTime;
 
 class TimeSlotService
 {
-    public function __construct(private RestaurantRepository $restaurantRepository, private ReservationRepository $reservationRepository)
+    public function __construct(private ReservationRepository $reservationRepository)
     {
     }
-    public function checkAvailable(Restaurant $restaurant, \DateTime $dateTime)
+    public function checkAvailable(Restaurant $restaurant, DateTime $dateTime): bool
     {
         $capacity = $restaurant->getCapacity();
-        $reservations = $this->reservationRepository->findBy([
-            'restaurant' => $restaurant,
-            'datetime' => $dateTime->format('y-m-d')
-        ]);
+
+        $reservations = $this->reservationRepository->findReservationByTimeSlot(
+            $restaurant,
+            $dateTime
+        );
 
         $total = 0;
         foreach ($reservations as $reservation) {
             $total += $reservation->getAdultNb() + $reservation->getKidNb();
         }
 
-        dd($total);
         return $capacity - $total > 0;
-
     }
-
 }
