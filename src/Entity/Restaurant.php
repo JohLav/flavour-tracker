17e53860b2b48fd2559cc1a5a6fee30ef97f9436
Guillaume <cvr.contact.pro@gmail.com>
2023-05-10 09:32:51 +0200

    Change reserv

--- src/Entity/Restaurant.php
+++ src/Entity/Restaurant.php
@@ -54,9 +54,6 @@ class Restaurant
     #[ORM\JoinColumn(nullable: false)]
     private ?City $city = null;
 
-    #[ORM\OneToMany(mappedBy: 'restaurant', targetEntity: Reservation::class)]
-    private Collection $reservations;
-
     public function __construct()
     {
         $this->users = new ArrayCollection();
@@ -336,36 +333,6 @@ class Restaurant
     public function setCity(?City $city): self
     {
         $this->city = $city;
-
-        return $this;
-    }
-
-    /**
-     * @return Collection<int, Reservation>
-     */
-    public function getReservations(): Collection
-    {
-        return $this->reservations;
-    }
-
-    public function addReservation(Reservation $reservation): self
-    {
-        if (!$this->reservations->contains($reservation)) {
-            $this->reservations->add($reservation);
-            $reservation->setRestaurant($this);
-        }
-
-        return $this;
-    }
-
-    public function removeReservation(Reservation $reservation): self
-    {
-        if ($this->reservations->removeElement($reservation)) {
-            // set the owning side to null (unless already changed)
-            if ($reservation->getRestaurant() === $this) {
-                $reservation->setRestaurant(null);
-            }
-        }
 
         return $this;
     }
