ec79ce192e688e4db6c92e5157651dc4ad2d69db
Johana Lavigne <johana.lavigne@gmail.com>
2023-11-07 17:46:55 +0100

    Corr: image.sql and user

--- data/image.sql
+++ data/image.sql
@@ -1,47 +1,47 @@
-INSERT INTO category (id, restaurant_id, url)
+INSERT INTO image (id, restaurant_id, url)
 VALUES (1, 1, 'https://drive.google.com/file/d/18GZkbRk6nK6fyYsv0hNzgBHstT-A3sYc/view?usp=drive_link');
-INSERT INTO category (id, restaurant_id, url)
+INSERT INTO image (id, restaurant_id, url)
 VALUES (2, 2, 'https://drive.google.com/file/d/1MTMnarJHBqPItSLN_zSa3rHJyD1IYTKf/view?usp=drive_link');
-INSERT INTO category (id, restaurant_id, url)
+INSERT INTO image (id, restaurant_id, url)
 VALUES (3, 3, 'https://drive.google.com/file/d/13GubKPTnNRxH0VILCMW6773HNnIAoWlG/view?usp=drive_link');
-INSERT INTO category (id, restaurant_id, url)
+INSERT INTO image (id, restaurant_id, url)
 VALUES (4, 4, 'https://drive.google.com/file/d/1tz5xiBZ2lj06mSpi9B4U1vke33qrDE73/view?usp=drive_link');
-INSERT INTO category (id, restaurant_id, url)
+INSERT INTO image (id, restaurant_id, url)
 VALUES (5, 5, 'https://drive.google.com/file/d/1Rv7yop4dKO_gb36Dg7OgQIqQr-qH4Oub/view?usp=drive_link');
-INSERT INTO category (id, restaurant_id, url)
+INSERT INTO image (id, restaurant_id, url)
 VALUES (6, 6, 'https://drive.google.com/file/d/1Su5e8EXmIauA4vEZJrvS9skVuusCXxcY/view?usp=drive_link');
-INSERT INTO category (id, restaurant_id, url)
+INSERT INTO image (id, restaurant_id, url)
 VALUES (7, 7, 'https://drive.google.com/file/d/1AdlfOHI0HuSuxbatuTKlPXgzyB18XA1l/view?usp=drive_link');
-INSERT INTO category (id, restaurant_id, url)
+INSERT INTO image (id, restaurant_id, url)
 VALUES (8, 8, 'https://drive.google.com/file/d/1Y6rlVbVFw_jfzsfT20g2pc-ZkGssHD72/view?usp=drive_link');
-INSERT INTO category (id, restaurant_id, url)
+INSERT INTO image (id, restaurant_id, url)
 VALUES (9, 1, 'https://drive.google.com/file/d/1zdGKZ_O_362fDwRpUXmXBz6J98dIskKE/view?usp=drive_link');
-INSERT INTO category (id, restaurant_id, url)
+INSERT INTO image (id, restaurant_id, url)
 VALUES (10, 2, 'https://drive.google.com/file/d/1P2wMkYgHbKI8ldDejHvWQBCxnhsYx_-y/view?usp=drive_link');
-INSERT INTO category (id, restaurant_id, url)
+INSERT INTO image (id, restaurant_id, url)
 VALUES (11, 2, 'https://drive.google.com/file/d/1ros-LB4_bkQb5TfDk2g3UunFu81cmUEX/view?usp=drive_link');
-INSERT INTO category (id, restaurant_id, url)
+INSERT INTO image (id, restaurant_id, url)
 VALUES (12, 2, 'https://drive.google.com/file/d/17YYAh0FbFBWv6o-2E1ysL9PJvy9MXaYD/view?usp=drive_link');
-INSERT INTO category (id, restaurant_id, url)
+INSERT INTO image (id, restaurant_id, url)
 VALUES (13, 2, 'https://drive.google.com/file/d/1Q7ioQ7ehQ4AY9n24DZ9Vy1e3SQGUDoQX/view?usp=drive_link');
-INSERT INTO category (id, restaurant_id, url)
+INSERT INTO image (id, restaurant_id, url)
 VALUES (14, 2, 'https://drive.google.com/file/d/1blGF1qwNpnds9K65GAQjZWsIq7OzroQP/view?usp=drive_link');
-INSERT INTO category (id, restaurant_id, url)
+INSERT INTO image (id, restaurant_id, url)
 VALUES (15, 3, 'https://drive.google.com/file/d/1SAda-_mLW4tmuNbPE7ClHb5QPiC6Zuhq/view?usp=drive_link');
-INSERT INTO category (id, restaurant_id, url)
+INSERT INTO image (id, restaurant_id, url)
 VALUES (16, 3, 'https://drive.google.com/file/d/1tpX2IF6hs0kWQ24l7_AQ4B46wAbaVgTF/view?usp=drive_link');
-INSERT INTO category (id, restaurant_id, url)
+INSERT INTO image (id, restaurant_id, url)
 VALUES (17, 4, 'https://drive.google.com/file/d/1yxRGCCamZ8fPoWhT0AajAuj0PyWVYnmW/view?usp=drive_link');
-INSERT INTO category (id, restaurant_id, url)
+INSERT INTO image (id, restaurant_id, url)
 VALUES (18, 5, 'https://drive.google.com/file/d/1fYEf2faNbJJbdoVuUs25yVagfC8HVdUY/view?usp=drive_link');
-INSERT INTO category (id, restaurant_id, url)
+INSERT INTO image (id, restaurant_id, url)
 VALUES (19, 6, 'https://drive.google.com/file/d/1GicngQjpWdO0XVtNt-zoM4f877sdBL9v/view?usp=drive_link');
-INSERT INTO category (id, restaurant_id, url)
+INSERT INTO image (id, restaurant_id, url)
 VALUES (20, 7, 'https://drive.google.com/file/d/1uMtrhUdUjSrRVlPItDAXPNwtQkAq4JFL/view?usp=drive_link');
-INSERT INTO category (id, restaurant_id, url)
+INSERT INTO image (id, restaurant_id, url)
 VALUES (21, 7, 'https://drive.google.com/file/d/1s4SUvvBWZWhIN6PHDaS7uzgeRnDdnAS_/view?usp=drive_link');
-INSERT INTO category (id, restaurant_id, url)
+INSERT INTO image (id, restaurant_id, url)
 VALUES (22, 8, 'https://drive.google.com/file/d/1IrRUCLuYbWd5zh5bcVcm3a3sRx9Tlk7l/view?usp=drive_link');
-INSERT INTO category (id, restaurant_id, url)
+INSERT INTO image (id, restaurant_id, url)
 VALUES (23, 8, 'https://drive.google.com/file/d/1dLNruwAWbpedd_OgHcKSHZJH1mzOEbPg/view?usp=drive_link');
 
