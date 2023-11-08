010ba1b5c2dbd8c2de772e24b46569b56ba4c8c0
Johana Lavigne <johana.lavigne@gmail.com>
2023-11-08 11:59:43 +0100

    Corr: routes pb and authorized users items.sql

--- data/item.sql
+++ data/item.sql
@@ -1,6 +1,28 @@
 INSERT INTO item (id, restaurant_id, name, description, price, category, visible)
-VALUES (2, 8, 'Quenelles Sauce Nantua', 'sauce écrevise', 14, '1', 1);
+VALUES (1, 1, 'Saucisson brioché', 'Saucisson', 13, '1', 1);
 INSERT INTO item (id, restaurant_id, name, description, price, category, visible)
-VALUES (3, 8, 'Ravioles du dauphiné', 'Pate, fromage, persil', 12, '1', 1);
+VALUES (2, 1, 'Quenelles Sauce Nantua', 'Sauce écrevisse', 14, '1', 1);
 INSERT INTO item (id, restaurant_id, name, description, price, category, visible)
-VALUES (4, 8, 'Saucisson brioché', 'Saucisson', 8, '1', 1);
+VALUES (3, 1, 'Ravioles du dauphiné', 'Pate, fromage, persil', 12, '1', 1);-
+INSERT INTO item (id, restaurant_id, name, description, price, category, visible)
+VALUES (4, 5, 'Dhal', 'Lentilles assaisonné de curry, curcuma et cumin', 12, '5', 1);
+INSERT INTO item (id, restaurant_id, name, description, price, category, visible)
+VALUES (5, 5, 'Poulet Tikka Masala', 'Sauce à base de curry et de tomates', 10, '5', 1);
+INSERT INTO item (id, restaurant_id, name, description, price, category, visible)
+VALUES (6, 5, 'Palak Paneer', 'Epinards, poivrons verts à base de fromage de soja', 10, '5', 1);
+INSERT INTO item (id, restaurant_id, name, description, price, category, visible)
+VALUES (7, 2, 'Ramen', 'Au choix : poulet, crevettes, porc ou boeuf', 14, '2', 1);
+INSERT INTO item (id, restaurant_id, name, description, price, category, visible)
+VALUES (8, 2, 'Porc sauce aigre douce', 'Filet mignon oignon, poivron vert et rouge', 18, '2', 1);
+INSERT INTO item (id, restaurant_id, name, description, price, category, visible)
+VALUES (9, 3, 'Pizza reine', 'Base tomates, mozzarella, jambons et champignons', 18, '3', 1);
+INSERT INTO item (id, restaurant_id, name, description, price, category, visible)
+VALUES (10, 4, 'Mafé de poulet', 'Plat de Côte d\'Ivoire au manioc, patates douces et poissons séchés', 18, '4', 1);
+INSERT INTO item (id, restaurant_id, name, description, price, category, visible)
+VALUES (12, 6, 'Shawarma', 'Fines tranches de viande d\'agneau, de boeuf ou de poulet grillées à la broche accompagnées de pain pita ou galettes et légumes frais', 18, '6', 1);
+INSERT INTO item (id, restaurant_id, name, description, price, category, visible)
+VALUES (13, 7, 'Taco', 'Tortilla de maïs garnie d\'ingrédients au choix (viandes ou légumes)', 18, '7', 1);
+INSERT INTO item (id, restaurant_id, name, description, price, category, visible)
+VALUES (14, 7, 'Quesadilla', 'Chausson composé d’une tortilla garnie de fromage, de haricots frits, de viande hachée, pliée en deux puis revenue à la plancha ou cuite sur un feu de bois', 18, '7', 1);
+INSERT INTO item (id, restaurant_id, name, description, price, category, visible)
+VALUES (15, 8, 'Tajine', 'Couscous cuisiné dans une terrine en argile. Préparation à la viande (poulet, agneau, boeuf), légumes et fruits secs', 18, '8', 1);
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -40,3 +40,4 @@ class HomeController extends AbstractController
         ]);
     }
 }
+
