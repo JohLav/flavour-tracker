ab730110448d77984f46228c6622ef75638b4e02
Johana Lavigne <johana.lavigne@gmail.com>
2023-11-10 19:15:44 +0100

    Fixed line break

--- src/Form/SearchType.php
+++ src/Form/SearchType.php
@@ -5,6 +5,7 @@
     use App\Entity\Category;
     use App\Entity\City;
     use App\Entity\Diet;
+    use App\Entity\Item;
     use Symfony\Bridge\Doctrine\Form\Type\EntityType;
     use Symfony\Component\Form\AbstractType;
     use Symfony\Component\Form\Extension\Core\Type\DateType;
