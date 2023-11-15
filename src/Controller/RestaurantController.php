74d0d2438ca6caf0eb8b1540849f34991b827cdc
Johana Lavigne <johana.lavigne@gmail.com>
2023-11-15 21:27:54 +0100

    Fixes code duplication in RestaurantController

--- src/Controller/RestaurantController.php
+++ src/Controller/RestaurantController.php
@@ -7,7 +7,6 @@
     use App\Repository\ImageRepository;
     use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
     use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-    use Symfony\Component\Form\FormInterface;
     use Symfony\Component\HttpFoundation\File\Exception\FileException;
     use Symfony\Component\HttpFoundation\Request;
     use Symfony\Component\HttpFoundation\Response;
