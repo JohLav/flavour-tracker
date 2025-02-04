8685de163a955ceaa65acdef57cf74f5410e2760
Johana Lavigne <johana.lavigne@gmail.com>
2025-02-04 14:32:20 +0100

    test: update

--- tests/Entity/RestaurantTest.php
+++ tests/Entity/RestaurantTest.php
@@ -1,15 +1,23 @@
 <?php
 
-    namespace App\Tests\Entity;
+namespace App\Tests\Entity;
 
-    use App\Entity\Restaurant;
-    use PHPUnit\Framework\TestCase;
+use App\Entity\Restaurant;
+use PHPUnit\Framework\TestCase;
 
 class RestaurantTest extends TestCase
 {
     public function testDefault(): void
     {
         $restaurant = new Restaurant();
-        $this->assertSame('Bouchon Lyonnais', $restaurant->getName());
+        $restaurant->setName('Bouchon lyonnais');
+        $restaurant->setPhone(123456789);
+        $restaurant->setCapacity(50);
+        $restaurant->setAddress('5 Rue des Gourmets, Lyon');
+
+        $this->assertSame('Bouchon lyonnais', $restaurant->getName());
+        $this->assertSame(123456789, $restaurant->getPhone());
+        $this->assertSame(50, $restaurant->getCapacity());
+        $this->assertSame('5 Rue des Gourmets, Lyon', $restaurant->getAddress());
     }
 }
